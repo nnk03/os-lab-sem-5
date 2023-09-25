37b89559fa0d0b39833ff155cfe1b073b231e05d
nnk03 <neerajnjps03@gmail.com>
2023-09-25 13:49:01 +0530

    stage-18 completed

--- stage-18/compile-all.sh
+++ stage-18/compile-all.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/zsh
 
 cd $HOME/myexpos/spl
-
+echo "Compiling SPL FILES \n"
 for i in $HOME/myexpos/stage-18/*.spl
 do
     echo $i
@@ -9,8 +9,16 @@ do
     ./spl $i
 done
 
+cd $HOME/myexpos/expl
+echo ""
+echo "Compiling EXPL FILES\n"
 
 
+for i in $HOME/myexpos/stage-18/*.expl
+do
+    echo $i
+    ./expl $i
+done
 
 
 
