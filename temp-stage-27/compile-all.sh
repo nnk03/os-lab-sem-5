d9c4a0a1dec1e0b305434cc2b5e54ee6111b58f5
nnk03 <neerajnjps03@gmail.com>
2023-11-12 00:21:48 +0530

    after a2-27.xsm, any command results in illegal memory access

---
+++ temp-stage-27/compile-all.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/zsh
+
+cd $HOME/myexpos/spl
+echo "Compiling SPL FILES"
+for i in $HOME/myexpos/temp-stage-27/*.spl
+do
+    echo $i
+    # pwd;
+    ./spl $i
+done
+
+cd $HOME/myexpos/expl
+echo "Compiling EXPL FILES"
+
+
+for i in $HOME/myexpos/temp-stage-27/*.expl
+do
+    echo $i
+    ./expl $i
+done
+
+
+
+
+
